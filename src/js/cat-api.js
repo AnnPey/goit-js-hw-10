e080e39f40b577235388e3f5cf1a4e5267ad60eb
AnnPey <peycheva440@gmail.ua>
2023-12-17 13:23:51 +0200

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
-  live_VWjU5t7VlUhO2ZrHmCZxWolJCkyoBvZF6zZyMkpMvxgJqGhRZqXAWAzHhEu3y3vK;
+  'live_VWjU5t7VlUhO2ZrHmCZxWolJCkyoBvZF6zZyMkpMvxgJqGhRZqXAWAzHhEu3y3vK';
 const catApiUrl = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
